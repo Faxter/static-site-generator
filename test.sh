fe088666d899f0a94a24317acccebf733cef4ad2
faxter <info@faxxter.com>
2025-11-08 15:20:04 +0100

    add test coverage capabilites

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
-python3 -m unittest discover -s test
+if [[ "$1" == "coverage" ]]; then
+    python3 -m coverage run -m unittest discover -s test
+    python3 -m coverage report
+else
+    python3 -m unittest discover -s test
+fi
