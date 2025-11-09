6ca1478773f5fe0d371e3bcbe257a5b732c06ee7
faxter <info@faxxter.com>
2025-11-09 15:05:22 +0100

    add copying of static contents for example tolkien page

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from pathlib import Path
+
+
+def copy_contents(src: Path, dst: Path):
+    if not os.path.exists(src):
+        raise Exception(f"source path {src} does not exist")
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    for item in os.listdir(src):
+        source = Path(os.path.join(src, item))
+        target = Path(os.path.join(dst, item))
+        if os.path.isfile(source):
+            shutil.copy(source, target)
+        else:
+            copy_contents(source, target)
 
 
 def main():
-    node = TextNode("some cool text", TextType.LINK, "https://faxxter.com")
-    print(node)
+    copy_contents(Path("static"), Path("public"))
 
 
 if __name__ == "__main__":
